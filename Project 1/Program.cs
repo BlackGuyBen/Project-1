15034e79e2ef5c574a85865f803fff475aaa6e38
Oluwalowo <olubeno@dunwoody.edu>
2018-01-25 08:55:01 -0600

    all names done

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -12,11 +12,25 @@ namespace Project_1
         {
 
             //Declarations
-            string[,] playerNames =
+            string[,] positions =
             {
-                {"Matthew", "Saqueon", "Mike"}
+                {"Quaterback", "Running Back", "Wide-Recevier", "Defensive Lineman",
+                 "Defensive-Back", "Tight Ends", "Line-Backers", "Offensive Tackles"}
 
             };
+            string[,] playerNames =
+            {
+             /*1*/   {"Mason Rudolph","Saquon Barkley","Courtland Sutton", "Maurice Hurst", "Joshua Jackson", "Mark Andrews", "Roquan Smith", "Orlando Brown"},
+             /*2*/   {"Lamar Jackson","Derrius Guice", "James Washington", "Vita Vea", "Derwin James", "Dallas Goedert", "Tremaine Edmunds", "Kolton Miller"},
+             /*3*/   {"Josh Rosen", "Bryce Love", "Marcell Ateman", "Taven Bryan", "Denzel Ward", "Jaylen Samuels", "Kendall Joseph", "Chukwuma Okorafor"},
+             /*4*/   {"Sam Darnold", "Ronald Jones II", "Anthony Miller", "Da'Ron Payne", "Minkah Fitzpatrick", "Mike Gesicki", "Dorian O'Daniel", "Connor Williams"},
+             /*5*/   {"Baker Mayfield","Damien Harris", "Calvin Ridley", "Harrison Phillips", "Isaiah Oliver","Troy Fumagalli","Malik Jefferson","Mike McGlinchey"}
+            };
+            string[,] colleges =
+            {
+             /*1*/  {"Oklahoma State", "Penn State", "Southern Methodist", "Michigan", "Iowa", "Oklahoma", "Georgia", "Oklahoma"},
+             /*2*/  {"Louisville", "LSU", "" }
+            }
 
         }
     }
