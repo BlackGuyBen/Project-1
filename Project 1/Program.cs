151b1b15ca739c72bd7fdf6bb960dd5426390d90
Oluwalowo <olubeno@dunwoody.edu>
2018-01-30 08:10:04 -0600

    all names, positions, and salaries done

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -29,8 +29,19 @@ namespace Project_1
             string[,] colleges =
             {
              /*1*/  {"Oklahoma State", "Penn State", "Southern Methodist", "Michigan", "Iowa", "Oklahoma", "Georgia", "Oklahoma"},
-             /*2*/  {"Louisville", "LSU", "" }
-            }
+             /*2*/  {"Louisville", "LSU", "Oklahoma State","Washington","Florida State","So. Dakota State","Virgina Tech","UCLA" },
+             /*3*/  {"UCLA","Stanford","Oklahoma State","Florida","Ohio State","NC State","Clemson","Western Michigan" },
+             /*4*/  {"Southern California","Southern California","Memphis","Alabama","Alabama","Penn State","Clemson","Texas" },
+             /*5*/  {"Oklahoma","Alabama","Alabama","Stanford","Colorado","Wisconsin","Texas","Notre Dame" }
+            };
+            string[,] salary =
+            {
+             /*1*/  {"$26,400,100","$24,500,100","$23,400,000","$26,200,300","$24,000,000","$27,800,900","$22,900,300","$23,000,000" },
+             /*2*/  {"$20,300,100","$19,890,200","$21,900,300","$22,000,000","$22,500,249","$21,000,800","$19,000,590","$20,000,000" },
+             /*3*/  {"$17,420,300","$18,700,800","$19,300,230","$16,000,000","$20,000,100","$17,499,233","$18,000,222","$19,400,000" },
+             /*4*/  {"$13,100,145","$15,000,000","$13,400,230","$18,000,000","$16,000,200","$27,900,200","$12,999,999","$16,200,700" },
+             /*5*/  {"$10,300,000","$11,600,400","$10,000,000","$13,000,000","$11,899,999","$14,900,333","$10,000,100","$15,900,000" }
+            };
 
         }
     }
